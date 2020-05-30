107a32a4ec7b4c31d11f46f159ad281a9199c5b2
Gabriel Lee <gabriellee@Gabriels-MacBook-Air.local>
2020-05-30 00:14:38 -0700

    Add argument requirements

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FILENAME=$(basename $0)
-HELP="Usage: \n${FILENAME} [-a] [-n] [-s] [-u username] [-p password] [-i destination_ip] [-m mount_path]\n -a\tSetup Apache\n -n\tSetup NFS\n -s\tSetup Samba\n -u\tUsername for login \n -p\tPassword for the login user\n -i\tDestination IP address\n -m\tMount path"
+HELP="Usage: \n${FILENAME} [-a] [-n] [-s] [-u username] [-p password] [-i destination_ip] [-m mount_path]\n -a\tSetup Apache\n -n\tSetup NFS\n -s\tSetup Samba\n -u\tUsername for login (Required: a, n, s)\n -p\tPassword for the login user (Required: a, s)\n -i\tDestination IP address (Required: n)\n -m\tMount path (Required: n, s)"
 
 PARAMS=""
 
